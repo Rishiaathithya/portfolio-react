023082ffa3bd353dd8c1b8fde805cf71a485025d
Rishiaathithya <rishiadhithya1997@gmail.com>
2025-02-06 22:02:50 +0530

    this is third commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,7 @@ import tier from '../assets/3-tier-app.png'
 
 const Projects = () => {
   return (
-    <section className=' text-white pt-[5%] mb-[10%]' id='projects'>
+    <section className=' text-white pt-[1%] mb-[10%]' id='projects'>
       <div className='text-center '>
         <h1 className='text-6xl font-semibold mb-8'>PROJECTS</h1>
         <span className='block bg-cyan-500 mt-4 mb-4 w-8 h-1 mx-auto '></span>
@@ -18,9 +18,9 @@ const Projects = () => {
         <div className=' testing component-1 m-[5%] border-2 border-gray-700 rounded-2xl bg-gray-900 flex flex-col p-[5%] hover:bg-transparent transform transition-transform hover:-translate-y-4 duration-500'>
           <img src={terraformimg} alt="" className='rounded-2xl'></img>
           <div className='tools-box flex text-violet-700 gap-x-6  m-[4%]'>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>docker</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>kubernates</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>jenkins</div>
+            <div className='rounded-3xl  flex  items-center text-sm lg:text-md '>docker</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>kubernates</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>jenkins</div>
           </div>
           <div className='text-box'>
             <h1 className='text-lg'>Terraform Initailaztion</h1>
@@ -31,9 +31,9 @@ const Projects = () => {
         <div className=' testing component-1 m-[5%] border-2 border-gray-700 rounded-2xl bg-gray-900 flex flex-col p-[5%] hover:bg-transparent transform transition-transform hover:-translate-y-4 duration-500'>
           <img src={spotify} alt="" className='rounded-2xl'></img>
           <div className='tools-box flex text-violet-700 gap-x-4 m-[4%]'>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>docker</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>kubernates</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>jenkins</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>docker</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>kubernates</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>jenkins</div>
           </div>
           <div className='text-box'>
             <h1 className='text-lg'>Spotify Terraform</h1>
@@ -44,9 +44,9 @@ const Projects = () => {
         <div className=' testing component-1 m-[5%] border-2 border-gray-700 rounded-2xl bg-gray-900 flex flex-col p-[5%] hover:bg-transparent transform transition-transform hover:-translate-y-4 duration-500'>
           <img src={netflix} alt="" className='rounded-2xl'></img>
           <div className='tools-box flex text-violet-700 gap-x-4 m-[4%]'>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>docker</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>kubernates</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>jenkins</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>docker</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>kubernates</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>jenkins</div>
           </div>
           <div className='text-box'>
             <h1 className='text-lg'>Netflix Deployment</h1>
@@ -57,9 +57,9 @@ const Projects = () => {
         <div className=' testing component-1 m-[5%] border-2 border-gray-700 rounded-2xl bg-gray-900 flex flex-col p-[5%] hover:bg-transparent transform transition-transform hover:-translate-y-4 duration-500'>
           <img src={portfolio} alt="" className='rounded-2xl'></img>
           <div className='tools-box flex text-violet-700 gap-x-4 m-[4%]'>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>docker</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>kubernates</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>jenkins</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>docker</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>kubernates</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>jenkins</div>
           </div>
           <div className='text-box'>
             <h1 className='text-lg'>React App</h1>
@@ -70,9 +70,9 @@ const Projects = () => {
         <div className=' testing component-1 m-[5%] border-2 border-gray-700 rounded-2xl bg-gray-900 flex flex-col p-[5%] hover:bg-transparent transform transition-transform hover:-translate-y-4 duration-500'>
           <img src={tier} alt="" className='rounded-2xl'></img>
           <div className='tools-box flex text-violet-700 gap-x-4 m-[4%]'>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>docker</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>kubernates</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>jenkins</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>docker</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>kubernates</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>jenkins</div>
           </div>
           <div className='text-box'>
             <h1 className='text-lg'>3-Tier Web Application</h1>
@@ -83,9 +83,9 @@ const Projects = () => {
         <div className=' testing component-1 m-[5%] border-2 border-gray-700 rounded-2xl bg-gray-900 flex flex-col p-[5%] hover:bg-transparent transform transition-transform hover:-translate-y-4 duration-500'>
           <img src={kubernates} alt="" className='rounded-2xl'></img>
           <div className='tools-box flex text-violet-700 gap-x-4 m-[4%]'>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>docker</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>kubernates</div>
-            <div className='rounded-2xl  flex justify-center items-center text-md '>jenkins</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>docker</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>kubernates</div>
+            <div className='rounded-2xl  flex  items-center text-sm lg:text-md '>jenkins</div>
           </div>
           <div className='text-box'>
             <h1 className='text-lg'>Kubernates Deployment</h1>
